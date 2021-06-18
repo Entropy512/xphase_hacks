1d82b5db2196103e3bdaacef7f3c88030c6f1c42
Andy Dodd <atd7@cornell.edu>
2021-06-18 13:41:34 -0400

    create_jpeg:  Don't create JPEGs with invalid R/G/B values for an xphase input
    Anything less than 23 is invalid - so instead of setting "black" as 0 (oops), set it to 24
    
    Results in much more consistent behavior when feeding synthetic JPEGs to PanoManager

--- create_jpeg.py
+++ create_jpeg.py
@@ -13,20 +13,22 @@ h = 2448
 
 imgdata = np.zeros((h,w,3),dtype=np.int8)
 for y in range(h):
-    val = 23 + int(np.floor(((230.0-23.0)*y)/h))
+    #val = 23 + int(np.floor(((230.0-23.0)*y)/h))
+    #val = 150
+    val = 24 + int(np.floor(((226.0-24.0)*y)/h))
     if(1):
         for x in range(w):
             if(x < w/4):
                 imgdata[y][x] = [val,val,val]
             elif(x < w/2):
-                imgdata[y][x] = [val, 0, 0]
+                imgdata[y][x] = [val, 24, 24]
             elif(x < 3*w/4):
-                imgdata[y][x] = [0, val, 0]
+                imgdata[y][x] = [24, val, 24]
             else:
-                imgdata[y][x] = [0, 0, val]
+                imgdata[y][x] = [24, 24, val]
     else:
         for x in range(w):
             imgdata[y][x] = [val, val, val]
 
-with open('multigrad.jpg', 'wb') as outfile:
+with open('multihue.jpg', 'wb') as outfile:
     outfile.write(jpeg.encode(imgdata,quality=99, jpeg_subsample=TJSAMP_420))
