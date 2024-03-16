007afeea640a4f92d5380e0a17b408c4dd0f1cd1
Andy Dodd <atd7@cornell.edu>
2024-03-16 13:36:39 -0400

    Update JPEG/DNG comparison tool
    Way too many changes to list...

--- cmp_jpeg_to_dng.py
+++ cmp_jpeg_to_dng.py
@@ -4,8 +4,12 @@ import rawpy
 from turbojpeg import TurboJPEG
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Cursor
 import argparse
 import os
+import colour
+from pprint import pprint
+import xphase_transfer
 
 jpeg = TurboJPEG()
 
@@ -21,11 +25,17 @@ dngname = filebase + '.dng'
 
 rawfile = rawpy.imread(dngname)
 
-jpgfile = open(args['input'], 'rb')
-planes = jpeg.decode_to_yuv_planes(jpgfile.read())
-yplane = planes[0]
-uplane = planes[1]
-vplane = planes[2]
+with open(args['input'], 'rb') as jpgfile:
+    planes = jpeg.decode_to_yuv_planes(jpgfile.read())
+    yplane = planes[0]
+    uplane = planes[1]
+    vplane = planes[2]
+
+with open(args['input'], 'rb') as jpgfile:
+    bgrdata = jpeg.decode(jpgfile.read())
+    rgbdata = bgrdata.copy()
+    rgbdata[:,:,0] = bgrdata[:,:,2]
+    rgbdata[:,:,2] = bgrdata[:,:,0]
 
 
 bayer_pattern = rawfile.raw_pattern
@@ -51,101 +61,163 @@ else:
     G_raw = raw_data[0:,0:,1]
     B_raw = raw_data[0:,0:,2]
 
-
-#we should check for nonequal shapes, but this is a very
-#specific data analysis script
-#h = jpgfile.shape[0]
-#w = jpgfile.shape[1]
-#lut = np.zeros(256)
-
-#for y in range(h):
-#    lut[R_jpg[y][0]] = R_raw[y][0]
-matrix_jpg2camrgb = np.array([[ 0.74222153,  0.14044039,  0.11733808],
-                              [ 0.20818888,  0.60000388,  0.19180724],
-                              [ 0.102492  ,  0.15562963,  0.74187837]])
-
-matrix_camrgb2srgb = np.linalg.inv(matrix_jpg2camrgb)
-
-lut = np.array([1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00,
-       1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00,
-       1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00,
-       1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00,
-       1.0000e+00, 1.0000e+00, 1.0000e+00, 1.0000e+00, 4.1000e+01,
-       8.7000e+01, 1.3400e+02, 1.8100e+02, 2.5100e+02, 2.9800e+02,
-       3.4500e+02, 3.9100e+02, 4.6100e+02, 5.0800e+02, 5.5500e+02,
-       6.2500e+02, 6.7200e+02, 7.4200e+02, 7.8900e+02, 8.5900e+02,
-       9.0600e+02, 9.7600e+02, 1.0460e+03, 1.1160e+03, 1.1630e+03,
-       1.2330e+03, 1.3030e+03, 1.3740e+03, 1.4440e+03, 1.5140e+03,
-       1.5840e+03, 1.6540e+03, 1.7240e+03, 1.8180e+03, 1.8880e+03,
-       1.9580e+03, 2.0520e+03, 2.1220e+03, 2.2160e+03, 2.2860e+03,
-       2.3790e+03, 2.4730e+03, 2.5430e+03, 2.6370e+03, 2.7300e+03,
-       2.8240e+03, 2.9170e+03, 3.0110e+03, 3.1040e+03, 3.1980e+03,
-       3.3150e+03, 3.4080e+03, 3.5020e+03, 3.6190e+03, 3.7120e+03,
-       3.8290e+03, 3.9230e+03, 4.0400e+03, 4.1570e+03, 4.2740e+03,
-       4.3910e+03, 4.5080e+03, 4.6240e+03, 4.7410e+03, 4.8580e+03,
-       4.9990e+03, 5.1160e+03, 5.2560e+03, 5.3730e+03, 5.5130e+03,
-       5.6540e+03, 5.7700e+03, 5.9110e+03, 6.0750e+03, 6.2150e+03,
-       6.3550e+03, 6.4960e+03, 6.6360e+03, 6.8000e+03, 6.9630e+03,
-       7.1040e+03, 7.2670e+03, 7.4310e+03, 7.5950e+03, 7.7580e+03,
-       7.9460e+03, 8.1090e+03, 8.2730e+03, 8.4600e+03, 8.6470e+03,
-       8.8110e+03, 8.9980e+03, 9.1850e+03, 9.3720e+03, 9.5590e+03,
-       9.7700e+03, 9.9800e+03, 1.0167e+04, 1.0378e+04, 1.0588e+04,
-       1.0799e+04, 1.1009e+04, 1.1243e+04, 1.1454e+04, 1.1664e+04,
-       1.1898e+04, 1.2132e+04, 1.2366e+04, 1.2600e+04, 1.2857e+04,
-       1.3091e+04, 1.3348e+04, 1.3582e+04, 1.3839e+04, 1.4097e+04,
-       1.4377e+04, 1.4634e+04, 1.4915e+04, 1.5172e+04, 1.5453e+04,
-       1.5734e+04, 1.6014e+04, 1.6318e+04, 1.6599e+04, 1.6903e+04,
-       1.7207e+04, 1.7511e+04, 1.7839e+04, 1.8143e+04, 1.8470e+04,
-       1.8797e+04, 1.9125e+04, 1.9452e+04, 1.9803e+04, 2.0154e+04,
-       2.0481e+04, 2.0832e+04, 2.1206e+04, 2.1557e+04, 2.1931e+04,
-       2.2306e+04, 2.2680e+04, 2.3077e+04, 2.3452e+04, 2.3849e+04,
-       2.4247e+04, 2.4668e+04, 2.5065e+04, 2.5486e+04, 2.5907e+04,
-       2.6352e+04, 2.6796e+04, 2.7217e+04, 2.7685e+04, 2.8129e+04,
-       2.8573e+04, 2.9041e+04, 2.9532e+04, 3.0000e+04, 3.0515e+04,
-       3.0982e+04, 3.1474e+04, 3.2011e+04, 3.2503e+04, 3.3017e+04,
-       3.3555e+04, 3.4070e+04, 3.4631e+04, 3.5169e+04, 3.5754e+04,
-       3.6291e+04, 3.6876e+04, 3.7461e+04, 3.8022e+04, 3.8630e+04,
-       3.9238e+04, 3.9846e+04, 4.0478e+04, 4.1109e+04, 4.1717e+04,
-       4.2396e+04, 4.3027e+04, 4.3682e+04, 4.4384e+04, 4.5038e+04,
-       4.5740e+04, 4.6442e+04, 4.7143e+04, 4.7845e+04, 4.8593e+04,
-       4.9342e+04, 5.0043e+04, 5.0839e+04, 5.1587e+04, 5.2359e+04,
-       5.3154e+04, 5.3949e+04, 5.4744e+04, 5.5563e+04, 5.6405e+04,
-       5.7223e+04, 5.8065e+04, 5.8931e+04, 5.9843e+04, 6.0685e+04,
-       6.1597e+04, 6.2509e+04, 6.3421e+04, 6.4333e+04, 6.5281e+04,
-       6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-       6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-       6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-       6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-       6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04, 6.5281e+04,
-       6.5281e+04])
-
-#print(repr(lut))
-ilut = np.arange(0.0,65281.0,1.0)
-ilut_flor = np.digitize(ilut,lut,right=True)
+# It seems like every third lens is flipped left/right and up/down by PanoManager
+# FIXME: handle this automatically
+# raw_data = np.fliplr(np.flipud(raw_data))
 
 
+lut = xphase_transfer.lut
 
-if(0):
-    plt.figure(1)
-    plt.imshow(np.power(np.matmul(jpg_expand,ccm)/65535.0,1/2.4))
-    plt.title('Corrected Input')
-    plt.figure(2)
-    plt.imshow(np.power(raw_data/65535.0,1/2.4))
-    plt.title('DNG with nonlinear transform')
-    plt.show()
-else:
-    plt.figure(1)
-    plt1 = plt.subplot(211)
-    plt1.plot(uplane[204*3,0::4], raw_data[408*3,0::8,0],'r')
-    plt1.plot(uplane[204*3,0::4], raw_data[408*4,0::8,1],'g')
-    plt1.plot(uplane[204*3,0::4], raw_data[408*3,0::8,2],'b')
-    plt2 = plt.subplot(212)
-    plt2.plot(vplane[204*3,0::4], raw_data[408*3,0::8,0],'r')
-    plt2.plot(vplane[204*3,0::4], raw_data[408*4,0::8,1],'g')
-    plt2.plot(vplane[204*3,0::4], raw_data[408*3,0::8,2],'b')
+print(np.amin(yplane))
+print(np.amax(yplane))
 
-    #plt.figure(2)
-    #plt.plot(jpgfile[0:,408*3,0],jpgfile[0:,408*3,0]/lkup_data[0:,408*3,0])
-    #plt.figure(3)
-    #plt.plot(R_raw/R_jpg)
-    plt.show()
+print(raw_data.shape)
+print(rgbdata.shape)
+
+lindata = lut[rgbdata]
+print(np.amax(lindata))
+print(np.amax(raw_data))
+#raw_data = ilut_flor[raw_data.astype(np.uint16)]
+
+#DNG color matrix
+color_matrix_raw = np.matrix([[1.69266987, -0.5626429913, -0.08418130087],
+                              [-0.3848780093, 1.108350039, 0.3184210059],
+                              [-0.0598646994, 0.1917970028, 1.04934001]])
+invmatrix_raw = np.linalg.inv(color_matrix_raw)
+primaries_raw = colour.primaries_whitepoint(invmatrix_raw)[0]
+whitepoint_raw = colour.primaries_whitepoint(invmatrix_raw)[1]
+colorspace_raw = colour.models.RGB_Colourspace('RAW color space', primaries_raw, whitepoint_raw, use_derived_matrix_RGB_to_XYZ=True, use_derived_matrix_XYZ_to_RGB=True)
+
+#Where the heck did I get this from???
+#Linearizing Zwikel's colorchecker shot and then dcamprofing it???
+#Or tracing the borders of a HALD-derived DNG that has no values below 23?
+primaries_jpeg = np.array([[0.567968, 0.368446],
+                           [0.30249, 0.64607],
+                           [0.1293, 0.006655]])
+whitepoint_jpeg = np.array([0.34567, 0.3585])
+colorspace_jpeg = colour.models.RGB_Colourspace('JPEG color space', primaries_jpeg, whitepoint_jpeg, use_derived_matrix_RGB_to_XYZ=True, use_derived_matrix_XYZ_to_RGB=True)
+
+raw_to_jpeg_matrix = colour.matrix_RGB_to_RGB(colorspace_raw, colorspace_jpeg)
+
+# Estimated color matrix from an earlier mode 0 run
+cmat_est1 = np.array([[0.74066847, 0.20708179, 0.10163293],
+                    [0.14307383, 0.62007526, 0.15846967],
+                    [0.11811859, 0.19158811, 0.73919268]])
+
+# Sanity check our matrices
+pprint(raw_to_jpeg_matrix)
+pprint(np.linalg.inv(raw_to_jpeg_matrix))
+pprint(cmat_est1)
+
+jpeg_to_raw_matrix = np.linalg.inv(raw_to_jpeg_matrix)
+
+mode = 2
+match mode:
+    case 0:
+        # Attempt to reverse engineer the color matrix that transforms from JPEG to RAW by fitting a matrix to
+        # the difference between linearized JPEG and RAW, works best with a HALD-ish image that only has values from
+        # 23 to 229.  (create.jpeg.py mode 2 with minval=23 and maxval=229)
+        # Trying to fit outside of the luma-only LUT region creates too many reverse engineering variables
+        ldata = np.reshape(lindata, (lindata.shape[0]*lindata.shape[1], lindata.shape[2]))
+        print(ldata.shape)
+        rdata = np.reshape(raw_data, (raw_data.shape[0]*raw_data.shape[1], raw_data.shape[2]))
+        cmat = np.matmul(np.matmul(np.linalg.inv(np.matmul(ldata.T,ldata)), ldata.T), rdata)
+
+        np.printoptions(legacy=False)
+        pprint(cmat_est1)
+        pprint(cmat)
+        print(np.amax(np.matmul(lindata,cmat)))
+        plt.figure()
+        plt.imshow(raw_data/65535.0)
+        plt.figure()
+        plt.imshow(lindata/65535.0)
+        plt.figure()
+        plt.imshow(np.matmul(lindata,cmat.T)/65535.0)
+        plt.figure()
+        print(np.amax(np.matmul(lindata,cmat)/raw_data))
+        print(np.amin(np.matmul(lindata,cmat)/raw_data))
+        print(np.mean(np.matmul(lindata,cmat)/raw_data))
+        plt.imshow(0.5*np.matmul(lindata,cmat)/raw_data)
+        plt.show()
+    case 1:
+        #really hackish things intended for images generated using create_jpeg_from_yuv.py
+        plt.figure()
+        plt1 = plt.subplot(211)
+        plt1.plot(uplane[204*3,0::4], raw_data[408*3,0::8,0],'r')
+        plt1.plot(uplane[204*3,0::4], raw_data[408*3,0::8,1],'g')
+        plt1.plot(uplane[204*3,0::4], raw_data[408*3,0::8,2],'b')
+        plt1.axhline(y=0, color='k', alpha=0.5)
+
+        plt2 = plt.subplot(212, sharex=plt1)
+        plt2.plot(uplane[204*3,0::4], rgbdata[408*3,0::8,0],'r')
+        plt2.plot(uplane[204*3,0::4], rgbdata[408*3,0::8,1],'g')
+        plt2.plot(uplane[204*3,0::4], rgbdata[408*3,0::8,2],'b')
+        plt2.axhline(y=0, color='k', alpha=0.5)
+        plt2.axhline(y=23, color='k', alpha=0.3)
+
+
+        plt.figure()
+        plt1 = plt.subplot(211)
+        plt1.plot(vplane[204*3,0::4], raw_data[408*3,0::8,0],'r')
+        plt1.plot(vplane[204*3,0::4], raw_data[408*3,0::8,1],'g')
+        plt1.plot(vplane[204*3,0::4], raw_data[408*3,0::8,2],'b')
+        plt1.axhline(y=0, color='k', alpha=0.5)
+        plt2 = plt.subplot(212, sharex=plt1)
+
+        plt2.plot(vplane[204*3,0::4], rgbdata[408*3,0::8,0],'r')
+        plt2.plot(vplane[204*3,0::4], rgbdata[408*3,0::8,1],'g')
+        plt2.plot(vplane[204*3,0::4], rgbdata[408*3,0::8,2],'b')
+        plt2.axhline(y=0, color='k', alpha=0.5)
+        plt2.axhline(y=23, color='k', alpha=0.3)
+
+        vslice = int(3264*(40/255))
+        plt.figure()
+        plt1 = plt.subplot(211)
+        plt1.plot(yplane[0::8,vslice], raw_data[0::8,vslice,0],'r')
+        plt1.plot(yplane[0::8,vslice], raw_data[0::8,vslice,1],'g')
+        plt1.plot(yplane[0::8,vslice], raw_data[0::8,vslice,2],'b')
+        plt2 = plt.subplot(212, sharex=plt1)
+        plt2.plot(yplane[0::8,vslice], rgbdata[0::8,vslice,0],'r')
+        plt2.plot(yplane[0::8,vslice], rgbdata[0::8,vslice,1],'g')
+        plt2.plot(yplane[0::8,vslice], rgbdata[0::8,vslice,2],'b')
+
+        plt.show()
+
+    case 2:
+        #Intended for images generated using create_jpeg.py modes 0 and 1
+        h = rgbdata.shape[0]
+        w = rgbdata.shape[1]
+        mod_data = np.matmul(raw_data, raw_to_jpeg_matrix.T)
+        for i in range(3):
+            plt.figure()
+            rdslice = mod_data[0::8,int((w/3)*i + w/6), :]
+            plt1 = plt.subplot(211)
+            plt1.plot(rdslice[:,0], 'r')
+            plt1.plot(rdslice[:,1], 'g')
+            plt1.plot(rdslice[:,2], 'b')
+            plt2 = plt.subplot(212, sharex=plt1)
+            plt2.plot(rgbdata[0::8,int((w/3)*i + w/6), 0], 'r')
+            plt2.plot(rgbdata[0::8,int((w/3)*i + w/6), 1], 'g')
+            plt2.plot(rgbdata[0::8,int((w/3)*i + w/6), 2], 'b')
+
+            plt.figure()
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 0], rdslice[:,0],'r')
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 1], rdslice[:,1],'g')
+            plt.plot(rgbdata[0::8,int((w/3)*i + w/6), 2], rdslice[:,2],'b')
+            x = np.linspace(0,255,2000)
+            plt.plot(x, xphase_transfer.linearize_code(x), 'k')
+            
+
+        w6 = int(w/6)
+        raw_data=raw_data[0::8,w6::2*w6,:]
+        raw_data = np.reshape(raw_data,(raw_data.shape[0]*raw_data.shape[1],raw_data.shape[2]))
+        lum_data = np.log2(np.amax(raw_data/65281.0,axis=1))
+        lumidx = np.argsort(lum_data)
+        raw_data = raw_data[lumidx]
+        lum_data = lum_data[lumidx]
+        print(lum_data.shape)
+        cplot = colour.plotting.plot_RGB_chromaticities_in_chromaticity_diagram_CIE1931(raw_data,colourspace=colorspace_raw,scatter_kwargs={'s':1,'c':lum_data},show=False)
+        cplot[0].colorbar(cplot[1].collections[2],ax=cplot[1])
+        cplot = colour.plotting.plot_planckian_locus_in_chromaticity_diagram_CIE1931(['A','D50','D65'],axes=cplot[1],show=False)
+        cplot = colour.plotting.plot_RGB_colourspaces_in_chromaticity_diagram_CIE1931([colorspace_raw, colorspace_jpeg, colour.models.RGB_COLOURSPACE_sRGB],axes=cplot[1],show=False)
+        plt.show()
